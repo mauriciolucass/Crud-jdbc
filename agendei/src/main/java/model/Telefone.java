a6930af3539fd189d855893cedf5a3ff0de89146
Administrador <Administrador@DESKTOP-34OROAS>
2022-10-05 11:44:34 -0300

    criando a calsse telefone

---
+++ agendei/src/main/java/model/Telefone.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Telefone {
+	private long id;
+	private String numero;
+	private String topo;
+	private long usuario;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getTopo() {
+		return topo;
+	}
+	public void setTopo(String topo) {
+		this.topo = topo;
+	}
+	public long getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(long usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+}
